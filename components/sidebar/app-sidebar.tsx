a3985bd8e21935dedddafdb2c10ceb8650806517
Mohtashim Ali <mohtashima85@gmail.com>
2026-02-17 22:00:20 +0500

    refactor: clean up AppSidebar and NavItem components for improved readability

--- components/sidebar/app-sidebar.tsx
+++ components/sidebar/app-sidebar.tsx
@@ -1,53 +1,34 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarHeader,
+	SidebarMenu,
 } from "@/components/ui/sidebar";
-import { Home, Wallet2Icon } from "lucide-react";
-import Link from "next/link";
+import AccountsList from "../accounts/accounts-list";
 import AddAccount from "../accounts/add-account";
-import AllAccounts from "../accounts/accounts-list";
-import AddTransaction from "../transactions/add-transaction";
-import NavItem from "./nav-item";
 
 export default function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader>
-        <div className="px-2 text-sm font-semibold">Personal Finance</div>
-      </SidebarHeader>
+	return (
+		<Sidebar>
+			<SidebarHeader>
+				<div className="px-2 text-sm font-semibold">Personal Finance</div>
+			</SidebarHeader>
 
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarMenu>
-            <NavItem href="/budget">
-              <Wallet2Icon />
-              <span>Budget</span>
-            </NavItem>
-            <NavItem href="/">
-              <Home />
-              <span>Home</span>
-            </NavItem>
-          </SidebarMenu>
-        </SidebarGroup>
-        <SidebarGroup>
-          <SidebarMenu>
-            <AllAccounts />
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarMenu>
+						<AccountsList />
+					</SidebarMenu>
+				</SidebarGroup>
+			</SidebarContent>
 
-      <SidebarFooter>
-        <div className="flex flex-col gap-2">
-          <AddAccount />
-          <AddTransaction />
-        </div>
-      </SidebarFooter>
-    </Sidebar>
-  );
+			<SidebarFooter>
+				<div className="flex flex-col gap-2">
+					<AddAccount />
+				</div>
+			</SidebarFooter>
+		</Sidebar>
+	);
 }
