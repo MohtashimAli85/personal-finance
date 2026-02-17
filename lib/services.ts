4c1996adc43dda54a67a13c5c737ed90888546f4
Mohtashim Ali <mohtashima85@gmail.com>
2026-02-17 21:56:12 +0500

    feat: add fetchAccounts and fetchCategories functions to retrieve data from API

--- lib/services.ts
+++ lib/services.ts
@@ -1,3 +1,6 @@
 import { fetchData } from "@/lib/helper";
 
 export const fetchAccounts = async () => fetchData<Account[]>("accounts");
+
+export const fetchCategories = async () =>
+	fetchData<GroupedCategory[]>("categories");
