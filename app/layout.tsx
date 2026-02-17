dd1ac91747a0155da3c248e7a6ffdc286a2dcafb
Mohtashim Ali <mohtashima85@gmail.com>
2026-02-17 22:48:29 +0500

    refactor: reorganize layout component structure and update metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import AppSidebar from "@/components/sidebar/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AppSidebar from "@/components/sidebar/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import StoreProvider from "@/context/store-context";
+import TransactionProvider from "@/context/transaction-context";
+import { fetchAccounts, fetchCategories } from "@/lib/services";
 import "./globals.css";
-import CacheProvider from "@/context/cache-context";
 
 const geistSans = Geist({
   variable: "--font-sans",
@@ -17,35 +19,39 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Personal Finance ",
-  description: "Made by Moshi, github.com/mohtashimali85",
+  description: "Made by Mohtashim, github.com/mohtashimali85",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const accounts = await fetchAccounts();
+  const groupedCategories = await fetchCategories();
   return (
-    <html lang="en" className={geistSans.variable + " dark"}>
+    <html lang="en" className={`${geistSans.variable} dark`}>
       <head>
         {/* {process.env.NODE_ENV === "development" && ( */}
+        {/*  eslint-disable-next-line @next/next/no-sync-scripts */}
         <script
-          defer
           crossOrigin="anonymous"
-          src="//unpkg.com/react-scan/dist/auto.global.js"
+          // src="//unpkg.com/react-scan/dist/auto.global.js"
         />
         {/* )} */}
       </head>
       <body className={`${geistMono.variable} antialiased`}>
-        <CacheProvider>
-          <SidebarProvider>
-            <AppSidebar />
-            <main className="min-h-screen bg-background w-full flex flex-col p-4 gap-2">
-              <SidebarTrigger />
-              {children}
-            </main>
-          </SidebarProvider>
-        </CacheProvider>
+        <StoreProvider initialData={{ accounts, groupedCategories }}>
+          <TransactionProvider>
+            <SidebarProvider>
+              <AppSidebar />
+              <main className="min-h-screen bg-background w-full flex flex-col p-4 gap-2">
+                <SidebarTrigger />
+                {children}
+              </main>
+            </SidebarProvider>
+          </TransactionProvider>
+        </StoreProvider>
       </body>
     </html>
   );
