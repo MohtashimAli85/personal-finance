229c4cb507b8ddb678cb66551e6e54f836326166
Mohtashim Ali <mohtashima85@gmail.com>
2026-02-18 21:00:35 +0500

    feat: add ComboboxTrigger to AccountCombobox and CategoryCombobox for improved accessibility

--- components/accounts/account-combobox.tsx
+++ components/accounts/account-combobox.tsx
@@ -1,67 +1,76 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import type { ComboboxProps } from "@/components/ui/combobox";
 import {
-	Combobox,
-	ComboboxContent,
-	ComboboxEmpty,
-	ComboboxInput,
-	ComboboxItem,
-	ComboboxList,
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+  ComboboxTrigger,
 } from "@/components/ui/combobox";
 import { useStore } from "@/context/store-context";
 
 const AccountCombobox = ({
-	value,
-	label,
-	onChange,
-	variant = "default",
-	modal = false,
+  value,
+  label,
+  onChange,
+  variant = "default",
+  modal = false,
 }: ComboboxProps) => {
-	const accounts = useStore((state) => state.accounts);
-	const [inputValue, setInputValue] = useState(label || "");
-	const items = accounts?.map((account) => ({
-		value: account.id,
-		label: account.name,
-	}));
-	return (
-		<Combobox
-			modal={modal}
-			required
-			id="account_id"
-			name="account_id"
-			value={{ value, label }}
-			onValueChange={(item) => {
-				if (!item) return;
-				if (item.value !== value && item.value) {
-					onChange?.(item.value);
-				}
-				setInputValue(item.label || "");
-			}}
-			items={items}
-		>
-			<ComboboxInput
-				showTrigger={false}
-				variant={variant}
-				placeholder="Select an Account"
-				value={inputValue}
-				onChange={(e) => setInputValue(e.target.value)}
-				onBlur={() => {
-					if (!inputValue) setInputValue(label || "");
-				}}
-			/>
-			<ComboboxContent>
-				<ComboboxEmpty>No Accounts found.</ComboboxEmpty>
-				<ComboboxList>
-					{(account) => (
-						<ComboboxItem key={account.value} value={account}>
-							{account.label}
-						</ComboboxItem>
-					)}
-				</ComboboxList>
-			</ComboboxContent>
-		</Combobox>
-	);
+  const accounts = useStore((state) => state.accounts);
+  const comboBtn = useRef<HTMLButtonElement>(null);
+  const [inputValue, setInputValue] = useState(label || "");
+  const items = accounts?.map((account) => ({
+    value: account.id,
+    label: account.name,
+  }));
+  return (
+    <Combobox
+      modal={modal}
+      required
+      id="account_id"
+      name="account_id"
+      value={{ value, label }}
+      onValueChange={(item) => {
+        if (!item) return;
+        if (item.value !== value && item.value) {
+          onChange?.(item.value);
+        }
+        setInputValue(item.label || "");
+      }}
+      items={items}
+    >
+      <ComboboxTrigger hidden ref={comboBtn} />
+      <ComboboxInput
+        showTrigger={false}
+        variant={variant}
+        placeholder="Select an Account"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onBlur={() => {
+          if (!inputValue) setInputValue(label || "");
+        }}
+        onClick={() => {
+          console.log("click");
+        }}
+        onFocus={() => {
+          comboBtn.current?.click();
+        }}
+      />
+      <ComboboxContent>
+        <ComboboxEmpty>No Accounts found.</ComboboxEmpty>
+        <ComboboxList>
+          {(account) => (
+            <ComboboxItem key={account.value} value={account}>
+              {account.label}
+            </ComboboxItem>
+          )}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox>
+  );
 };
 
 export default AccountCombobox;
--- components/categories/category-combobox.tsx
+++ components/categories/category-combobox.tsx
@@ -1,73 +1,77 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import {
-	Combobox,
-	ComboboxCollection,
-	ComboboxContent,
-	ComboboxEmpty,
-	ComboboxGroup,
-	ComboboxInput,
-	ComboboxItem,
-	ComboboxLabel,
-	ComboboxList,
-	type ComboboxProps,
-	ComboboxSeparator,
+  Combobox,
+  ComboboxCollection,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxGroup,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxLabel,
+  ComboboxList,
+  type ComboboxProps,
+  ComboboxSeparator,
+  ComboboxTrigger,
 } from "@/components/ui/combobox";
 import { useStore } from "@/context/store-context";
 
 const CategoryCombobox = ({ value, label, onChange }: ComboboxProps) => {
-	const groupedCategories = useStore((state) => state.groupedCategories);
-	const [inputValue, setInputValue] = useState(label ?? "");
+  const comboBtn = useRef<HTMLButtonElement>(null);
+  const groupedCategories = useStore((state) => state.groupedCategories);
+  const [inputValue, setInputValue] = useState(label ?? "");
 
-	return (
-		<Combobox
-			id="categoryId"
-			name="categoryId"
-			value={{ value, label }}
-			onValueChange={(item) => {
-				if (!item) return;
-				if (item.value !== value && item.value) {
-					onChange?.(item.value);
-				}
-				setInputValue(item.label ?? "");
-			}}
-			items={groupedCategories}
-		>
-			<ComboboxInput
-				variant={"table"}
-				showTrigger={false}
-				placeholder="Select a Category"
-				value={inputValue}
-				onChange={(e) => setInputValue(e.target.value)}
-				onBlur={() => {
-					if (!inputValue) setInputValue(label ?? "");
-				}}
-			/>
-			<ComboboxContent>
-				<ComboboxEmpty>No Categories found.</ComboboxEmpty>
-				<ComboboxList>
-					{(group, index) => (
-						<ComboboxGroup key={group.id} items={group.categories}>
-							<ComboboxLabel>{group.name}</ComboboxLabel>
-							<ComboboxCollection>
-								{(category) => (
-									<ComboboxItem
-										key={category.id}
-										value={{ value: category.id, label: category.name }}
-									>
-										{category.name}
-									</ComboboxItem>
-								)}
-							</ComboboxCollection>
-							{index < (groupedCategories?.length ?? 0) - 1 && (
-								<ComboboxSeparator />
-							)}
-						</ComboboxGroup>
-					)}
-				</ComboboxList>
-			</ComboboxContent>
-		</Combobox>
-	);
+  return (
+    <Combobox
+      id="categoryId"
+      name="categoryId"
+      value={{ value, label }}
+      onValueChange={(item) => {
+        if (!item) return;
+        if (item.value !== value && item.value) {
+          onChange?.(item.value);
+        }
+        setInputValue(item.label ?? "");
+      }}
+      items={groupedCategories}
+    >
+      <ComboboxTrigger hidden ref={comboBtn} />
+      <ComboboxInput
+        variant={"table"}
+        showTrigger={false}
+        placeholder="Select a Category"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onBlur={() => {
+          if (!inputValue) setInputValue(label ?? "");
+        }}
+        onFocus={() => comboBtn.current?.click()}
+      />
+      <ComboboxContent>
+        <ComboboxEmpty>No Categories found.</ComboboxEmpty>
+        <ComboboxList>
+          {(group, index) => (
+            <ComboboxGroup key={group.id} items={group.categories}>
+              <ComboboxLabel>{group.name}</ComboboxLabel>
+              <ComboboxCollection>
+                {(category) => (
+                  <ComboboxItem
+                    key={category.id}
+                    value={{ value: category.id, label: category.name }}
+                  >
+                    {category.name}
+                  </ComboboxItem>
+                )}
+              </ComboboxCollection>
+              {index < (groupedCategories?.length ?? 0) - 1 && (
+                <ComboboxSeparator />
+              )}
+            </ComboboxGroup>
+          )}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox>
+  );
 };
 
 export default CategoryCombobox;
