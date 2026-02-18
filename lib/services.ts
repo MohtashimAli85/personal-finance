71f464de9ccf18da1857715b7eeec386daab1289
Mohtashim Ali <mohtashima85@gmail.com>
2026-02-18 21:01:47 +0500

    feat: add fetchTransactions function to retrieve transactions with parameters

--- lib/services.ts
+++ lib/services.ts
@@ -4,3 +4,7 @@ export const fetchAccounts = async () => fetchData<Account[]>("accounts");
 
 export const fetchCategories = async () =>
 	fetchData<GroupedCategory[]>("categories");
+
+export const fetchTransactions = async (
+	params: Record<string, string | number | undefined>,
+) => fetchData<TransactionRow[]>("transactions", params);
