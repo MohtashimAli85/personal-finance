f868e9a6eed49e0958ac93f316752bd01b68a53a
Mohtashim Ali <mohtashima85@gmail.com>
2026-02-19 16:10:55 +0500

    refactor: improve code formatting and readability in AccountItem component

--- components/accounts/account-item.tsx
+++ components/accounts/account-item.tsx
@@ -9,50 +9,51 @@ import AccountContextMenu from "./account-context-menu";
 import CloseAccount from "./close-account";
 
 export const AccountItem = ({ account }: { account: Account }) => {
-	const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
 
-	const handleBlur = () => {
-		const inputEl = ref.current?.children[0] as HTMLInputElement;
-		const labelEl = ref.current?.children[1] as HTMLSpanElement;
+  const handleBlur = () => {
+    const inputEl = ref.current?.children[0] as HTMLInputElement;
+    const labelEl = ref.current?.children[1] as HTMLSpanElement;
 
-		inputEl.setAttribute("hidden", "true");
-		labelEl.removeAttribute("hidden");
-	};
-	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-		if (e.key === "Enter" || e.key === "NumpadEnter") {
-			const value = (e.target as HTMLInputElement).value.trim();
-			if (value) {
-				e.currentTarget.form?.requestSubmit();
-				updateAccount(account.id, value);
-				const labelEl = ref.current?.children[1] as HTMLSpanElement;
-				labelEl.textContent = value;
-			}
-			handleBlur();
-		}
-	};
-	return (
-		<Dialog>
-			<AccountContextMenu ref={ref}>
-				<SidebarMenuItem>
-					<NavItem href={`/transactions?accountId=${account.id}`}>
-						<div className="grow max-w-30" ref={ref}>
-							<input
-								hidden
-								id={"name"}
-								defaultValue={account.name}
-								className="outline-none"
-								onBlur={handleBlur}
-								onKeyDown={handleKeyDown}
-							/>
-							<span ref={ref}>{account.name}</span>
-						</div>
-						<SidebarMenuBadge>
-							{formatCurrency(account.balance)}
-						</SidebarMenuBadge>
-					</NavItem>
-				</SidebarMenuItem>
-			</AccountContextMenu>
-			<CloseAccount account={account} />
-		</Dialog>
-	);
+    inputEl.setAttribute("hidden", "true");
+    inputEl.value = account.name;
+    labelEl.removeAttribute("hidden");
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" || e.key === "NumpadEnter") {
+      const value = (e.target as HTMLInputElement).value.trim();
+      if (value) {
+        e.currentTarget.form?.requestSubmit();
+        updateAccount(account.id, value);
+        const labelEl = ref.current?.children[1] as HTMLSpanElement;
+        labelEl.textContent = value;
+      }
+      handleBlur();
+    }
+  };
+  return (
+    <Dialog>
+      <AccountContextMenu ref={ref}>
+        <SidebarMenuItem>
+          <NavItem href={`/transactions?accountId=${account.id}`}>
+            <div className="grow max-w-30" ref={ref}>
+              <input
+                hidden
+                id={"name"}
+                defaultValue={account.name}
+                className="outline-none"
+                onBlur={handleBlur}
+                onKeyDown={handleKeyDown}
+              />
+              <span>{account.name}</span>
+            </div>
+            <SidebarMenuBadge>
+              {formatCurrency(account.balance)}
+            </SidebarMenuBadge>
+          </NavItem>
+        </SidebarMenuItem>
+      </AccountContextMenu>
+      <CloseAccount account={account} />
+    </Dialog>
+  );
 };
