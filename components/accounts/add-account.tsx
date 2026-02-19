d06a01ce9dc302b52ec2c0c39393e03cee8b0d0b
Mohtashim Ali <mohtashima85@gmail.com>
2026-02-19 22:44:00 +0500

    refactor: improve code formatting and readability in AddAccount component

--- components/accounts/add-account.tsx
+++ components/accounts/add-account.tsx
@@ -6,13 +6,13 @@ import { Controller, useForm } from "react-hook-form";
 import { createAccount } from "@/app/actions/accounts/mutations";
 import { Button } from "@/components/ui/button";
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { useMutation } from "@/hooks/use-mutation";
@@ -21,82 +21,90 @@ import { SidebarMenuButton } from "../ui/sidebar";
 import { type AccountValues, accountSchema, type SchemaInput } from "./schema";
 
 const initialState = {
-	message: "",
-	success: false,
+  message: "",
+  success: false,
 };
 const AddAccount = () => {
-	const [open, setOpen] = useState(false);
-	const [state, mutate] = useMutation<AccountValues>(
-		createAccount,
-		initialState,
-	);
-	const form = useForm<SchemaInput, unknown, AccountValues>({
-		resolver: zodResolver(accountSchema),
-		defaultValues: { name: "", initialBalance: "0" },
-		errors: state?.errors,
-	});
-	const onSubmit = (values: AccountValues) => {
-		if (state.shouldValidate) {
-			const isNameUnchanged = values.name === state.payload?.name;
-			if (isNameUnchanged) {
-				form.setError("name", {
-					message: state.errors?.name?.message,
-				});
-				return;
-			}
-		}
-		mutate(values, {
-			onSuccess: () => {
-				setOpen(false);
-			},
-		});
-	};
-	return (
-		<Dialog open={open} onOpenChange={setOpen}>
-			<DialogTrigger asChild>
-				<SidebarMenuButton>
-					<Plus /> Add Account
-				</SidebarMenuButton>
-			</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Add a new account</DialogTitle>
-					<DialogDescription>
-						Fill in the details below to create a new account.
-					</DialogDescription>
-				</DialogHeader>
-				<form onSubmit={form.handleSubmit(onSubmit)}>
-					<FieldGroup className="group">
-						<Controller
-							name="name"
-							control={form.control}
-							render={({ field, fieldState }) => (
-								<Field data-invalid={fieldState.invalid}>
-									<FieldLabel>Name</FieldLabel>
-									<Input {...field} aria-invalid={fieldState.invalid} />
-									<FieldError>{fieldState.error?.message}</FieldError>
-								</Field>
-							)}
-						/>
-						<Controller
-							name="initialBalance"
-							control={form.control}
-							render={({ field, fieldState }) => (
-								<Field data-invalid={fieldState.invalid}>
-									<FieldLabel>Balance</FieldLabel>
-									<Input {...field} aria-invalid={fieldState.invalid} />
-									<FieldError>{fieldState.error?.message}</FieldError>
-								</Field>
-							)}
-						/>
-						<DialogFooter>
-							<Button type="submit">Create Account</Button>
-						</DialogFooter>
-					</FieldGroup>
-				</form>
-			</DialogContent>
-		</Dialog>
-	);
+  const [open, setOpen] = useState(false);
+  const [state, mutate] = useMutation<AccountValues>(
+    createAccount,
+    initialState,
+  );
+  const form = useForm<SchemaInput, unknown, AccountValues>({
+    resolver: zodResolver(accountSchema),
+    defaultValues: { name: "", initialBalance: "0" },
+    errors: state?.errors,
+  });
+  const onSubmit = (values: AccountValues) => {
+    if (state.shouldValidate) {
+      const isNameUnchanged = values.name === state.payload?.name;
+      if (isNameUnchanged) {
+        form.setError("name", {
+          message: state.errors?.name?.message,
+        });
+        return;
+      }
+    }
+    mutate(values, {
+      onSuccess: () => {
+        setOpen(false);
+      },
+    });
+  };
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={(value) => {
+        setOpen(value);
+        setTimeout(() => {
+          form.reset();
+        }, 300);
+      }}
+    >
+      <DialogTrigger asChild>
+        <SidebarMenuButton>
+          <Plus /> Add Account
+        </SidebarMenuButton>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a new account</DialogTitle>
+          <DialogDescription>
+            Fill in the details below to create a new account.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup className="group">
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Name</FieldLabel>
+                  <Input {...field} aria-invalid={fieldState.invalid} />
+                  <FieldError>{fieldState.error?.message}</FieldError>
+                </Field>
+              )}
+            />
+            <Controller
+              name="initialBalance"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Balance</FieldLabel>
+                  <Input {...field} aria-invalid={fieldState.invalid} />
+                  <FieldError>{fieldState.error?.message}</FieldError>
+                </Field>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Create Account</Button>
+            </DialogFooter>
+          </FieldGroup>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default AddAccount;
